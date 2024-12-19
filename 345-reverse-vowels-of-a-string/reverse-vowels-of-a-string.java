cd12ca7e7e6b523309d025a55a8a8c434e80c166
Slot0359 <chutchayut.app@gmail.com>
2024-12-19 18:30:56 +0700

    Time: 11 ms (17.46%) | Memory: 45.1 MB (46.04%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,27 +1,26 @@
 class Solution {
     public String reverseVowels(String s) {
         StringBuilder sb = new StringBuilder(s);
-        HashMap<Character, Boolean> map = new HashMap<>();
         Stack<Character> vowelsStack = new Stack<>();
         Queue<Integer> indexQueue = new LinkedList<>();
+        ArrayList<Character> vowelsList = new ArrayList<>();
 
-        map.put('a', true);
-        map.put('e', true);
-        map.put('i', true);
-        map.put('o', true);
-        map.put('u', true);
+        vowelsList.add('a');
+        vowelsList.add('e');
+        vowelsList.add('i');
+        vowelsList.add('o');
+        vowelsList.add('u');
 
-        map.put('A', true);
-        map.put('E', true);
-        map.put('I', true);
-        map.put('O', true);
-        map.put('U', true);
-
+        vowelsList.add('A');
+        vowelsList.add('E');
+        vowelsList.add('I');
+        vowelsList.add('O');
+        vowelsList.add('U');
 
         for (int i = 0; i < s.length(); i++) {
             char letter = s.charAt(i);
 
-            if (map.containsKey(letter)) {
+            if (vowelsList.contains(letter)) {
                 vowelsStack.add(letter);
                 indexQueue.add(i);
             }
