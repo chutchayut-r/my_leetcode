05db07141eebe08968bc77a018d3a297617350fa
Chutchayut Rongkum <chutchayut.app@gmail.com>
2024-12-30 02:19:14 +0700

    Time: 1 ms (93.43%) | Memory: 41.7 MB (28.79%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,10 +1,13 @@
 class Solution {
-   public boolean isSubsequence(String s, String t) {
+    public boolean isSubsequence(String s, String t) {
+        char[] s_ary = s.toCharArray();
+        char[] t_ary = t.toCharArray();
+        
         int t_point = 0;
         int s_point = 0;
 
         while (t_point < t.length() && s_point < s.length()) {
-            if (s.charAt(s_point) == t.charAt(t_point)) {
+            if (s_ary[s_point] == t_ary[t_point]) {
                 s_point++;
             }
             t_point++;
