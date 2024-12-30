80a9acab85756ee1b064e46bec2d3bd875ff90e5
Chutchayut Rongkum <chutchayut.app@gmail.com>
2024-12-30 22:17:23 +0700

    Time: 5 ms (74.33%) | Memory: 58.3 MB (11.96%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -3,13 +3,14 @@ class Solution {
         int max_area = 0;
         int l = 0;
         int r = height.length-1;
-        
+
         while (l < r) {
-             
-            int area = Math.min(height[l],height[r]) * (r-l);
-            
+
+            int minHeight = Math.min(height[l], height[r]); 
+            int area = minHeight * (r - l);
+
             max_area = Math.max(area, max_area);
-            
+
             if (height[l] >= height[r]) {
                 r--;
             } else {
@@ -19,5 +20,4 @@ class Solution {
         
         return max_area;
     }
-
 }
