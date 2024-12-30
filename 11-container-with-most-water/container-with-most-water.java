59ed7fc8fa4b8473c77961e3067be6f6d1a44f8d
Chutchayut Rongkum <chutchayut.app@gmail.com>
2024-12-30 22:13:37 +0700

    Time: 5 ms (74.33%) | Memory: 58.3 MB (11.96%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -4,7 +4,7 @@ class Solution {
         int l = 0;
         int r = height.length-1;
         
-        while (l != r) {
+        while (l < r) {
              
             int area = Math.min(height[l],height[r]) * (r-l);
             
