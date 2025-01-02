96be4f21d1650a59eb5d1b609a19eb532b5ffb66
Chutchayut Rongkum <chutchayut.app@gmail.com>
2025-01-02 19:54:31 +0700

    Time: 5 ms (99.30%) | Memory: 45.1 MB (19.07%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -2,14 +2,22 @@ class Solution {
     public int maxVowels(String s, int k) {
         int maxNumVowel = 0;
         char[] chars = s.toCharArray();
-        
-        for (int index = 0; index < k; index++) {
-            if (this.isVowel(chars[index])) {
-                maxNumVowel++;
-            }
+        // a (byte = 97) to z (byte = 122)
+        int[] vowelMap = new int[123];
+        int curNumVowel = 0;
+
+        vowelMap['a'] = 1;
+        vowelMap['e'] = 1;
+        vowelMap['i'] = 1;
+        vowelMap['o'] = 1;
+        vowelMap['u'] = 1;
+        // default value of int is 0.
+
+        for (int index =0; index<k; index++) {
+            curNumVowel += vowelMap[chars[index]];
+            maxNumVowel = curNumVowel;
         }
-        
-        int curNumVowel = maxNumVowel;
+
 
         for (int round = 0; round < s.length()-k; round++) {
 
@@ -17,23 +25,18 @@ class Solution {
                 return  k;  // maxNumVomel cannot better than K value.
             }
 
-
             char removeChar = chars[round];
             char addChar = chars[round+k];
 
-            if(isVowel(removeChar)){
-                curNumVowel--;
-            }
-            if (isVowel(addChar)) {
-                curNumVowel++;
-            }
+            curNumVowel -= vowelMap[removeChar];
+            curNumVowel += vowelMap[addChar];
 
-           maxNumVowel = maxNumVowel < curNumVowel ? curNumVowel:maxNumVowel;
+            maxNumVowel = maxNumVowel < curNumVowel ? curNumVowel:maxNumVowel;
         }
 
         return maxNumVowel;
     }
-    
+
     public boolean isVowel(char ch) {
         return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
     }
