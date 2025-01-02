8dfdbc7ad4ec7d27864d82fb80f1813823920cf0
Chutchayut Rongkum <chutchayut.app@gmail.com>
2025-01-03 02:50:15 +0700

    Time: 3 ms (86.42%) | Memory: 46.8 MB (32.86%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,38 +1,26 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int sum = 0;
-        int output = k;
-        int windowSize;
-        int windowSum;
+        int length = nums.length;
+        int left = 0;
+        int right= 0;
+        int maxLen = 0;
 
-        for (windowSize = nums.length; windowSize > k; windowSize--) {
-
-            windowSum = calSum(nums, windowSize);
-
-            if (windowSum >= windowSize-k) {
-                return windowSize;
+        for (right = 0; right < length; right++) {  // expand the window
+            if (nums[right] == 0) { // flips zero.
+                k--;
             }
 
-            for (int index=0; index+windowSize < nums.length; index++) {
-                windowSum -= nums[index];
-                windowSum += nums[index+windowSize];
-
-                if (windowSum >= windowSize-k) {
-                    return windowSize;
+            if (k<0) {  // shrink window
+                if (nums[left] == 0) {  // Now,zero is outside a window. So,increase k value
+                    k++;
                 }
+                left++;
             }
+
+            int diff = right-left+1;
+            maxLen = diff > maxLen ? diff : maxLen;
         }
 
-        return output;
-    }
-
-    public int calSum(int[] num, int lastIndex) {
-        int sum = 0;
-
-        for (int i=0; i < lastIndex; i++)  {
-            sum += num[i];
-        }
-
-        return sum;
+        return maxLen;
     }
 }
