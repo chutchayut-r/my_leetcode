f4105e20cb24d23b5da75fa68845a8be3f7a1d06
Chutchayut Rongkum <chutchayut.app@gmail.com>
2025-01-04 19:48:49 +0700

    Time: 10 ms (74.25%) | Memory: 45.5 MB (42.73%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -1,31 +1,27 @@
 class Solution {
-    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        HashMap<Integer,Boolean> map1 = new HashMap<>();
-        HashMap<Integer,Boolean> map2 = new HashMap<>();
-
+   public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
         List<List<Integer>> output = new ArrayList<>();
         Set<Integer> list1 = new HashSet<>();
         Set<Integer> list2 = new HashSet<>();
 
         for (int num : nums1) {  // Map of num1
-            map1.put(num,true);
+            list1.add(num);
         }
 
         for (int num : nums2) { // Map of num2
-            map2.put(num,true);
+            list2.add(num);
         }
 
-
-        for (int num : nums1) {  // Check num1 with Map2
-            if (!map2.containsKey(num)){
-                list1.add(num);
+        for (int num : nums2) {  // Check num1 with Map2
+            if (list1.contains(num)){
+                list1.remove(num);
             }
         }
         output.add(new ArrayList<>(list1));
 
-        for (int num : nums2) { // Check num2 with Map1
-            if (!map1.containsKey(num)){
-                list2.add(num);
+        for (int num : nums1) { // Check num2 with Map1
+            if (list2.contains(num)){
+                list2.remove(num);
             }
         }
         output.add(new ArrayList<>(list2));
